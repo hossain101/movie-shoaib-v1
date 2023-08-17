7aa96e7def873b1098462540f9b84df69d1430e2
shoaib <shoaibhossain24@gmail.com>
2023-08-17 07:42:27 +0600

    remove ngrok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import api from './api/axiosConfig';
-import { useState, useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
 
 function App() {
-
-const [movies, setMovies] = useState([]);
-
+  const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
-    const response = await api.get('/api/v1/movies');
-    setMovies(response.data);
-  }
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-  return (
-    
-    
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://9c96-103-106-239-104.ngrok.io",
-    headers: {"ngok-skip-browser-warning": "true"}
+  baseURL: "http://localhost:8080",
 });
-
-
-
